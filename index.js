0d8efc8bc74ff620bec94dd1326982e99c746306
Gonçalo Morais <gnclmorais@gmail.com>
2015-03-16 11:34:34 -0400

    Update error message.
    Names were the other way around, it was `country-data` that got deprecated in favour of `country-data`.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 var countries = require('country-data').countries.all2
 
-throw new Error('`country-data` module is deprecated in favor of `country-codes` module at <https://github.com/OpenBookPrices/country-data/>.')
+throw new Error('`country-codes` module is deprecated in favor of `country-data` module at <https://github.com/OpenBookPrices/country-data/>.')
 
 module.exports = countries
